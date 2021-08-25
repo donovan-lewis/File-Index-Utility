5859abb11968591fdeafc75fe315b0ebf42233c3
donovan-lewis <85753791+donovan-lewis@users.noreply.github.com>
2021-08-25 10:54:03 -0400

    Add files via upload

---
+++ CSVCreator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace CSVCreator
+{
+    class Program
+    {
+        static string csvLOC = "";
+        static string csvName = "DirIndex.csv";
+        static string line = "";
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            Console.Write("Please input the csv save point (Do not include a file just directory): ");
+            csvLOC = Console.ReadLine();
+            
+            Console.Write("Please input the start point (DO NOT INCLUDE \\): ");
+            string path = Console.ReadLine();
+            DirectoryInfo start = new DirectoryInfo(path);
+            DirSearch(start);
+            Console.WriteLine($"Saving file to {csvLOC}\\{csvName}...");
+            File.WriteAllText($"{csvLOC}\\{csvName}", line);
+            Console.WriteLine($"File Saved To: {csvLOC}\\{csvName}... Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void DirSearch(DirectoryInfo source)
+        {
+            count++;
+            Console.WriteLine($"Directory Count: {count}");
+            line += $"\n{source.FullName}";
+            try
+            {
+                foreach (FileInfo file in source.GetFiles())
+                    line += $",{file.Name}";
+            
+            foreach (DirectoryInfo dir in source.GetDirectories())
+                DirSearch(dir);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine($"Access Forbidden! {source.FullName}");
+            }
+        }
+    }
+}
